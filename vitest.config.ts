cc7656f7a96961842b9e8f70bd798817a268d028
Bruno Zorić <bruno.zoric@gmail.com>
2026-05-07 17:46:29 +0200

    chore: ci hardening (#3)

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,7 +13,13 @@ export default defineConfig({
         coverage: {
             provider: "v8",
             include: ["src/**/*.ts"],
-            exclude: ["**/__tests__/**", "**/index.ts", "**/abstractions/**"]
+            exclude: ["**/__tests__/**", "**/index.ts", "**/abstractions/**"],
+            thresholds: {
+                statements: 90,
+                branches: 80,
+                functions: 90,
+                lines: 90
+            }
         }
     }
 });
